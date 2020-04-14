78c284a570885e0c466fd5538904d3c5fb65f4c2
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-04-14 15:20:22 -0700

    lru cache class after writing method signatures

--- LRUcache.py
+++ LRUcache.py
@@ -3,3 +3,12 @@ class LRU:
     def __init__(self, capacity) :
         self.capacity = capacity
         self.lis = []
+    
+    def put(self, value):
+        pass
+
+    def get(self, index):
+        pass
+
+    def get_cache(self) :
+        pass
