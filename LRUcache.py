35d4a3ba391e088a7888794b3d69a679776d5fbb
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-04-14 16:11:03 -0700

    completed lru implementation

--- LRUcache.py
+++ LRUcache.py
@@ -5,10 +5,37 @@ class LRU:
         self.lis = []
     
     def put(self, value):
-        pass
+        if not value in self.lis and len(self.lis) < self.capacity :
+            self.lis.append(value)
+        
+        elif not value in self.lis and len(self.lis) >= self.capacity :
+            self.lis.remove(self.lis[0])
+            self.lis.append(value)
+        else :
+            self.lis.remove(value)
+            self.lis.append(value)
+        return
+
 
     def get(self, index):
-        pass
+        if index > self.capacity :
+            return -1
+        else :
+            return self.lis[index]
 
     def get_cache(self) :
-        pass
+        return self.lis
+
+# def main():
+#     lru = LRU(5)
+#     # lru.put(1)
+#     # lru.put(2)
+#     lru.put(3)
+#     lru.put(4)
+#     lru.put(5)
+#     lru.put(6)
+#     # lru.put(4)
+#     lru.put(4)
+#     print(lru.get_cache())
+# if __name__ == "__main__":
+#     main()
--- LRUtest.py
+++ LRUtest.py
@@ -7,19 +7,22 @@ def main():
     lru.put(1)
     lru.put(2)
     lru.put(3)
-
-    assert lru.get_cache() == "[1, 2, 3]", "put method failed"
+    assert lru.get_cache() == [1, 2, 3], "put method failed"
     print("put method passed")
 
-    lru.put(4)
     lru.put(5)
+    lru.put(3)
 
-    assert lru.get(2) == "3", "get method failed"
+    assert lru.get(2) == 5, "get method failed"
     print("get method passed")
 
+    assert lru.get(6) == -1, "get method with index greater than capacity failed"
+    print("get method with index greater than capacity passed")
+
     lru.put(6)
-    lru.get_cache() == "[2, 3, 4, 5, 6]", "get_cache method failed"
+    lru.get_cache() == [2, 3, 4, 5, 6], "get_cache method failed"
     print("get_cache method passed")
 
+    print("lru implementation completed")
 if __name__ == "__main__":
     main()
